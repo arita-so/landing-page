ef010ee65a572491a769f601b980a5d8b34df92f
Ana Rita <114316027+arita-so@users.noreply.github.com>
2023-05-30 23:56:06 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,28 @@
+const carrega = document.getElementById("carrega")
+carrega.addEventListener ('click', (event) => {
+    alert ("Obrigada por visitares a minha Landing Page para o programa Step By Tech!") 
+});
+
+// Funções para o MouseOver Event
+
+function changeBackground(){
+    document.getElementById("aprender").style.color = "white";
+    document.getElementById("aprender").style.backgroundColor = "#ff6585";
+    
+}
+
+function backToNormal(){
+    document.getElementById("aprender").style.color = "#ff6585";
+    document.getElementById("aprender").style.backgroundColor = "";
+    
+}
+
+function changeBackground1(){
+   document.getElementById("grupo").style.color = "white";
+    document.getElementById("grupo").style.backgroundColor = "#ff6585";
+}
+
+function backToNormal1(){
+    document.getElementById("grupo").style.color = "#ff6585";
+    document.getElementById("grupo").style.backgroundColor = "";
+}
